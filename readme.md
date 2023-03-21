d5822743a5f1e3dc423848fb5b9f0bca9f75c497
TTsdzb <ttsdzb@outlook.com>
2023-03-21 11:07:18 +0800

    feat: initial commit

---
+++ readme.md
@@ -0,0 +1,9 @@
+# koishi-plugin-ip-query
+
+[![npm](https://img.shields.io/npm/v/koishi-plugin-ip-query?style=flat-square)](https://www.npmjs.com/package/koishi-plugin-ip-query)
+
+使用 Alapi 查询 IP 地址的归属地信息。
+
+## 使用
+
+该插件依赖于 <https://github.com/TTsdzb/koishi-plugin-alapi>， 使用一个半免费 API 平台 [Alapi](https://alapi.cn) 来进行查询，你需要注册一个账号并获取自己的 token，填入 alapi 插件的配置中。当你准备好后，启用插件，发送 `ip` 命令即可查询。
---
+++ src/index.ts
@@ -0,0 +1,24 @@
+import { Context, Schema } from "koishi";
+
+export const name = "ip-query";
+
+export interface Config {}
+
+export const Config: Schema<Config> = Schema.object({});
+
+export const using = ["alapi"] as const;
+
+export function apply(ctx: Context) {
+  ctx
+    .command("ip <ip:string>", "查询IP地址归属地")
+    .example("ip 1.1.1.1  查询1.1.1.1的归属地信息")
+    .action(async (_, ip) => {
+      try {
+        let result = await ctx.alapi.request("ip", { ip });
+        return `${result.data.ip}：${result.data.pos}`;
+      } catch (e) {
+        if (e instanceof Error) return "IP查询时出现错误：" + e.message;
+        else return "IP查询时出现未知错误";
+      }
+    });
+}
